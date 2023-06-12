78216c15ae6e5eb5729dd59a07bb0f78fdef721f
unknown <merabtodua7@gmail.com>
2023-06-12 23:13:58 +0400

    Vowel remover

--- 8kyu/Sum of differences in array.js
+++ 8kyu/Sum of differences in array.js
@@ -63,3 +63,19 @@ function pillars(numPill, dist, width) {
 
 //    i can use in this example Math.max(...pillars.) or Math.min()
 //    )
+
+// Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
+
+// Examples
+// "hello"     -->  "hll"
+// "codewars"  -->  "cdwrs"
+// "goodbye"   -->  "gdby"
+// "HELLO"     -->  "HELLO"
+// don't worry about uppercase vowels
+// y is not considered a vowel for this kata
+
+function shortcut (string) {
+  let vowels=["a", "e", "i", "o", "u"]
+ return string.split('').filter(x=>!vowels.includes(x)).join('')
+ 
+ }
