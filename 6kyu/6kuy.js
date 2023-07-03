146d6990b64a7ac1921affda6e5c262f0f03d948
unknown <merabtodua7@gmail.com>
2023-07-03 19:12:02 +0400

    adding 6kyu

--- 6kyu/6kuy.js
+++ 6kyu/6kuy.js
@@ -95,3 +95,10 @@ function findOddOccurrence(arr) {
   }
   return result;
 }
+
+// Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+
+// It should remove all values from list a, which are present in list b keeping their order.
+function arrayDiff(a, b) {
+  return a.filter(x => !b.includes(x));
+}
